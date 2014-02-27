aa62282a4165a801502b6fcff2e7bd3e66a070c2
Satoshi KOJIMA <skoji@mac.com>
2014-02-28 07:12:57 +0900

    remove requires

--- lib/arti_mark.rb
+++ lib/arti_mark.rb
@@ -1,21 +1,5 @@
 # -*- encoding: utf-8 -*-
 require "arti_mark/version"
-require "arti_mark/command_lexer"
-require "arti_mark/base_parser"
-require 'arti_mark/common_block_parser'
-require "arti_mark/paragraph_parser"
-require "arti_mark/div_parser"
-require "arti_mark/article_parser"
-require "arti_mark/section_parser"
-require "arti_mark/head_parser"
-require "arti_mark/block_image_parser"
-require "arti_mark/list_parser"
-require "arti_mark/ordered_list_parser"
-require "arti_mark/unordered_list_parser"
-require "arti_mark/definition_list_parser"
-require "arti_mark/universal_block_parser"
-require 'arti_mark/syntax'
-
 require 'arti_mark/html/generator'
 require 'arti_mark/parser'
 
