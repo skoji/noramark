d1c0debf27884b72f8856fb99b236b23d3f19d63
Satoshi KOJIMA <skoji@mac.com>
2014-05-08 07:52:48 +0900

    remove htmlbook : create dependent noramark-htmlbook project

--- lib/nora_mark.rb
+++ lib/nora_mark.rb
@@ -3,8 +3,10 @@ require 'nora_mark/node_util'
 require 'nora_mark/transformer'
 require 'nora_mark/parser'
 require 'nora_mark/node_set'
-require 'nora_mark/document'
 require 'nora_mark/html/generator'
+require 'nora_mark/document'
+require 'nora_mark/extensions'
+
 
 
 
--- lib/nora_mark/extensions.rb
+++ lib/nora_mark/extensions.rb
@@ -10,7 +10,7 @@ module NoraMark
     def load_generator(generator)
       module_name = '::NoraMark::#{generator.to_s.capitalize}::Generator'
       return const_get(module_name) if const_defined? module_name.to_sym
-      path = "noramark-generator-#{generator.to_s.lowercase}.rb"
+      path = "#{generator.to_s.lowercase}.rb"
       current_dir_path = File.expand_path(File.join('.', '.noramark-plugins', path))
       home_dir_path = File.expand_path(File.join(ENV['HOME'], '.noramark-plugins', path))
       if File.exist? current_dir_path
@@ -18,9 +18,9 @@ module NoraMark
       elsif File.exist? home_dir_path
         require home_dir_path
       else
-        require path
+        require "noramark/#{generator.to_s.lowercase}"
       end
-      const_defined? module_name.to_sym ? const_get(module_name) : nil
+      const_defined?(module_name.to_sym) ? const_get(module_name) : nil
     end
   end
 end
