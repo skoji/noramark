cfbb1ababf06d0f6767aea13146443177a9f2f36
Satoshi KOJIMA <skoji@mac.com>
2014-08-23 13:31:50 +0900

    fix #19 add rake task

---
+++ lib/nora_mark/rake_task.rb
@@ -0,0 +1,55 @@
+require 'nora_mark'
+require 'rake/tasklib'
+
+module NoraMark
+  class RakeTask < ::Rake::TaskLib
+    attr_accessor :lang
+    def initialize(lang: 'en')
+      @preprocessors = []
+      @transformers = []
+      yield self if block_given?
+      @lang ||= lang
+      define
+    end
+
+    def add_preprocessor(&block)
+      @preprocessors << block;
+    end
+
+    def add_transformer(&block)
+      @transformers << block;
+    end
+
+    def define
+      desc "rule for *-nora.txt to *-nora_xxx.html. Use *-nora-transform.rb on same directory as transformer"
+      rule( /-((nora)|(arti))_[0-9]{3}\.xhtml/ =>
+            proc {|task_name|
+              task_name.sub(/^(.+?-((nora)|(arti)))_[0-9]{3}\.xhtml/, '\1.txt')
+            }) do
+        |t|
+
+        dir =  File.dirname File.expand_path(t.source)
+        transformer_name = File.join dir, File.basename(t.source, '.txt') + '-transform.rb'
+        nora = NoraMark::Document.parse(
+                                        File.open(t.source),
+                                        :lang => @lang,
+                                        :document_name=>t.name.sub(/_[0-9]{3}\.xhtml/, '')) do
+          |doc|
+          @preprocessors.each do
+            |prepro|
+            doc.preprocessor(&prepro)
+          end
+          @transformers.each do
+            |transformer|
+            doc.add_transformer(&transformer)
+          end
+          if File.exist? transformer_name
+            doc.add_transformer(text: File.open(transformer_name).read)
+          end
+        end
+        nora.html.write_as_files
+      end
+      
+    end
+  end
+end
